64b6f8a3ac675e13385cb1ff708bcaff28269bb2
Ee Yieng Zheng <eeyiengz@hotmail.com>
2017-10-19 22:41:00 -0700

    played around with ResultSet and ResultSetMetaData

--- src/DBConnectionTest.java
+++ src/DBConnectionTest.java
@@ -7,18 +7,34 @@ public class DBConnectionTest {
 
         Statement stmt = null;
         ResultSet rs = null;
-        ResultSetMetaData rsmd = null;
+        ResultSetMetaData rsmd;
         try {
             // get connection with username = args[0] password = args[1]
             Connection conn = DatabaseConnection.getConnection(args[0], args[1], args[2]);
             DatabaseMetaData meta = conn.getMetaData();
-            // stmt = conn.createStatement();
-            // stmt.setFetchSize(1000);
-            // rs = stmt.executeQuery("SELECT * FROM classics");
-            // rsmd = rs.getMetaData();
+            stmt = conn.createStatement();
+            stmt.setFetchSize(1000);
+            rs = stmt.executeQuery("SELECT * FROM classics");
+            rsmd = rs.getMetaData();
 
             System.out.println("Connection to " + args[0] + " successful");
             System.out.println("Driver Name : " + meta.getDriverName());
+
+            // get DATABASE name
+            ResultSet dbName = stmt.executeQuery("SELECT DATABASE()");
+            dbName.next();
+            System.out.println("Inside " + dbName.getString(1) + "." + rsmd.getTableName(1));
+
+            // printing out everything from the SELECT query
+            int columnsNumber = rsmd.getColumnCount();
+            while (rs.next()) {
+                for (int i = 1; i <= columnsNumber; i++) {
+                    if (i > 1) System.out.print(",  ");
+                    String columnValue = rs.getString(i);
+                    System.out.print(rsmd.getColumnName(i) + " = " + columnValue);
+                }
+                System.out.println("");
+            }
 
         } catch (SQLException ex) {
             System.out.println("SQLException: " + ex.getMessage());
@@ -28,7 +44,7 @@ public class DBConnectionTest {
             if (rs != null) {
                 try {
                     rs.close();
-                } catch (SQLException sqlEx) {
+                } catch (SQLException ignored) {
                 } // ignore
 
                 rs = null;
@@ -37,7 +53,7 @@ public class DBConnectionTest {
             if (stmt != null) {
                 try {
                     stmt.close();
-                } catch (SQLException sqlEx) {
+                } catch (SQLException ignored) {
                 } // ignore
 
                 stmt = null;
