3d43d1fa9894c3239510722252255eda7243faf7
Ee Yieng Zheng <eeyiengz@hotmail.com>
2017-11-26 22:54:04 -0800

    sped up database import

--- cs157a_schema.sql
+++ cs157a_schema.sql
@@ -22,21 +22,46 @@ CREATE TABLE users (
   user_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
   username VARCHAR(128) NOT NULL,
   password VARCHAR(128) NOT NULL,
+  PRIMARY KEY (user_id),
+  UNIQUE KEY (username)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS users_detail;
+CREATE TABLE users_detail (
+  user_detail_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  user_id INT UNSIGNED NOT NULL,
   email VARCHAR(128) DEFAULT NULL,
   first_name VARCHAR(64) DEFAULT NULL,
   last_name VARCHAR(64) DEFAULT NULL,
-  PRIMARY KEY (user_id)
+  age TINYINT UNSIGNED DEFAULT NULL,
+  nickname VARCHAR(128) DEFAULT NULL,
+  PRIMARY KEY (user_detail_id),
+  UNIQUE KEY (email),
+  INDEX (user_id),
+  FOREIGN KEY (user_id)
+    REFERENCES users (user_id)
+    ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS students;
 CREATE TABLE students (
   student_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
   user_id INT UNSIGNED NOT NULL,
-  age TINYINT UNSIGNED DEFAULT NULL,
-  nickname VARCHAR(128) DEFAULT NULL,
-  PRIMARY KEY  (student_id)
+  PRIMARY KEY (student_id),
+  FOREIGN KEY (user_id)
+    REFERENCES users (user_id)
+    ON DELETE CASCADE ON UPDATE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+DROP TABLE IF EXISTS professors;
+CREATE TABLE professors (
+  professor_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  user_id INT UNSIGNED NOT NULL,
+  PRIMARY KEY (professor_id),
+  FOREIGN KEY (user_id)
+    REFERENCES users (user_id)
+    ON DELETE CASCADE ON UPDATE CASCADE
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- departments, courses, buildings, rooms, etc.
@@ -50,7 +75,7 @@ CREATE TABLE courses (
   course_description TEXT DEFAULT NULL,
   FULLTEXT idx_course_description (course_description),
   PRIMARY KEY (course_id),
-  UNIQUE KEY (course_number),
+  UNIQUE KEY (course_number, dept_id),
   INDEX (dept_id),
   FOREIGN KEY (dept_id)
     REFERENCES departments(dept_id)
@@ -63,7 +88,7 @@ CREATE TABLE departments (
   dept_short_name VARCHAR(8) NOT NULL,
   dept_name VARCHAR(128) NOT NULL,
   PRIMARY KEY (dept_id),
-  UNIQUE KEY (dept_short_name)
+  UNIQUE KEY (dept_short_name, dept_name)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
